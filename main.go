1945e4f26f43fea20195aa72d17022206a578b18
ni-sgn <saganeli.nika@gmail.com>
2024-02-14 23:22:27 +0400

    update

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/speaker"
 )
 
 func main() {
-	speaker.Init(beep.SampleRate(48000), 512)
-	streamer, err := (&GeneralSound{SampleRate: 48000, Frequency: 27, Gain: -.9, Phase: 0}).GenerateSound()
 
+	sr := beep.SampleRate(48000)
+
+	speaker.Init(sr, 512)
+	defer speaker.Close()
+
+	streamer, err := (&GeneralSound{SampleRate: 48000, Frequency: 440, Gain: -.9, Phase: 0}).GenerateSound()
 	if err != nil {
 		panic(err)
 	}
+	limitedStreamer := beep.Take(sr.N(5*time.Second), streamer)
 
-	speaker.PlayAndWait(streamer)
-	defer speaker.Close()
+	first, second := beep.Dup(limitedStreamer)
+	_, fourth := beep.Dup(first)
+
+	//_ := sync.WaitGroup{}
+	Save(fourth, sr, "out_fourth.wav")
+	/*
+		wg.Add(1)
+		go func() {
+			Save(third, sr, "out_third.wav")
+			wg.Done()
+		}()
+
+		wg.Add(1)
+		go func() {
+			Save(first, sr, "out_first.wav")
+			wg.Done()
+		}()
+	*/
+
+	//wg.Wait()
+
+	speaker.PlayAndWait(second)
+
+	fmt.Println("The stream has ended")
 }
--- saveSound.go
+++ saveSound.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gopxl/beep"
+	"github.com/gopxl/beep/wav"
 )
 
-func Save(stream beep.Streamer, filename string) {
+func Save(stream beep.Streamer, sr beep.SampleRate, filename string) error {
+
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+
+	err = wav.Encode(f, stream, beep.Format{SampleRate: 48000, NumChannels: 2, Precision: 3})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
